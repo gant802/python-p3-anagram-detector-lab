2a8eaab6583c40cb3d153b88c412c37fbffecb35
Grant Cummings <grantcummings4429@gmail.com>
2024-05-15 08:23:50 -0400

    finished exercise

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,27 @@
-# your code goes here!
+import ipdb
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        def refactor(word):
+            word_to_refactor = list(word)
+            word_to_refactor.sort()
+            refactored_word = "".join(word_to_refactor)
+            return refactored_word
+
+        new_word_to_compare = refactor(self.word)
+        matched_word = []
+        
+        for word in word_list:
+            new_word = refactor(word)
+            if new_word == new_word_to_compare:
+                matched_word.append(word)
+        return matched_word
+            
+            
+
+
+
+
